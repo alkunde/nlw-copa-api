4178999bbeeb3a3603b44b02c261230fb65b3b4f
André Kunde <andre.kunde@portoseguro.com.br>
2023-09-19 12:58:55 -0300

    project begin

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      avatarUrl: 'https://github.com/alkunde.png',
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        }
+      }
+    }
+  })
+
+  const game = await prisma.game.create({
+    data: {
+      date: '2023-09-20T12:00:00.000Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2023-09-21T14:00:00.000Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
